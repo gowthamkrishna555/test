180214a20220e62e01c407e864f944b06ea2c1a8
Gowtham Krishna <117144261+gowthamkrishna555@users.noreply.github.com>
2025-02-24 09:46:56 +0530

    Sequelize ssl

--- lib/sequelize.ts
+++ lib/sequelize.ts
@@ -1,29 +1,35 @@
 import { Sequelize } from "sequelize";
-import pg from 'pg';
+import pg from "pg";
 
 const sequelize = new Sequelize(
   "neondb", // Database name
-  "neondb_owner",  // Database user
+  "neondb_owner", // Database user
   "npg_H5BKa6EwhvFl", // Database password
   {
-    host: "ep-purple-wildflower-a1gtfkf9-pooler.ap-southeast-1.aws.neon.tech", // Change if your database is on another server
-    dialect: "postgres", // Specify PostgreSQL
+    host: "ep-purple-wildflower-a1gtfkf9-pooler.ap-southeast-1.aws.neon.tech", 
+    dialect: "postgres",
     dialectModule: pg,
-    logging: true // Set to true for debugging queries
+    logging: false, // Set to true for debugging queries
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Allows self-signed certificates
+      },
+    },
   }
 );
 
 export const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync(); 
-    console.log(" PostgreSQL connected successfully!");
+    await sequelize.sync();
+    console.log("PostgreSQL connected successfully!");
   } catch (error) {
-    console.error(" PostgreSQL connection failed:", error);
+    console.error("PostgreSQL connection failed:", error);
     process.exit(1);
   }
 };
 
-connectDB(); 
+connectDB();
 
-export { sequelize }; 
+export { sequelize };
